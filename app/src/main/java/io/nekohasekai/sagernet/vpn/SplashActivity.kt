2adf44f61a4dfd171acd8f4c04437dada8fa6e3a
Ca3tiel <ahmad.mosallanezhad@gmail.com>
2023-09-25 20:28:43 +0330

    load servers in splash screen

--- app/src/main/java/io/nekohasekai/sagernet/vpn/SplashActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/vpn/SplashActivity.kt
@@ -4,27 +4,81 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import io.nekohasekai.sagernet.GroupType
 import io.nekohasekai.sagernet.R
+import io.nekohasekai.sagernet.database.DataStore
+import io.nekohasekai.sagernet.database.GroupManager
+import io.nekohasekai.sagernet.database.ProxyGroup
+import io.nekohasekai.sagernet.database.SubscriptionBean
+import io.nekohasekai.sagernet.group.GroupUpdater
+import io.nekohasekai.sagernet.ktx.applyDefaultValues
+import io.nekohasekai.sagernet.ktx.runOnDefaultDispatcher
 
 class SplashActivity : AppCompatActivity() {
 
-    private val SPLASH_DELAY: Long = 1000 // 1 seconds in milliseconds
+    fun ProxyGroup.init() {
+        DataStore.groupName = name ?: "HolyVpn"
+        DataStore.groupType = 1
+        DataStore.groupOrder = order
+        DataStore.groupIsSelector = isSelector
+
+        DataStore.frontProxy = frontProxy
+        DataStore.landingProxy = landingProxy
+        DataStore.frontProxyTmp = if (frontProxy >= 0) 3 else 0
+        DataStore.landingProxyTmp = if (landingProxy >= 0) 3 else 0
+
+        val subscription = subscription ?: SubscriptionBean().applyDefaultValues()
+        DataStore.subscriptionLink = "https://panel.holyip.workers.dev/link/9RTsfMryrGwgWZVb48eN?config=1"
+        DataStore.subscriptionForceResolve = subscription.forceResolve
+        DataStore.subscriptionDeduplication = subscription.deduplication
+        DataStore.subscriptionUpdateWhenConnectedOnly = subscription.updateWhenConnectedOnly
+        DataStore.subscriptionUserAgent = subscription.customUserAgent
+        DataStore.subscriptionAutoUpdate = subscription.autoUpdate
+        DataStore.subscriptionAutoUpdateDelay = subscription.autoUpdateDelay
+    }
+
+    fun ProxyGroup.serialize() {
+        name = DataStore.groupName.takeIf { it.isNotBlank() } ?: "My group"
+        type = DataStore.groupType
+        order = DataStore.groupOrder
+        isSelector = DataStore.groupIsSelector
+
+        frontProxy = if (DataStore.frontProxyTmp == 3) DataStore.frontProxy else -1
+        landingProxy = if (DataStore.landingProxyTmp == 3) DataStore.landingProxy else -1
+
+        val isSubscription = type == GroupType.SUBSCRIPTION
+        if (isSubscription) {
+            subscription = (subscription ?: SubscriptionBean().applyDefaultValues()).apply {
+                link = DataStore.subscriptionLink
+                forceResolve = DataStore.subscriptionForceResolve
+                deduplication = DataStore.subscriptionDeduplication
+                updateWhenConnectedOnly = DataStore.subscriptionUpdateWhenConnectedOnly
+                customUserAgent = DataStore.subscriptionUserAgent
+                autoUpdate = DataStore.subscriptionAutoUpdate
+                autoUpdateDelay = DataStore.subscriptionAutoUpdateDelay
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Hide the Action Bar
-        supportActionBar?.hide()
         setContentView(R.layout.activity_splash)
 
         // Use a Handler to post a delayed Runnable
         Handler().postDelayed({
-            // After the delay, start the main activity
+            // After the delay, start the WelcomeActivity
             val intent = Intent(this, WelcomeActivity::class.java)
             startActivity(intent)
 
-            // Finish the splash activity to prevent returning to it later
+            // Finish the current activity to prevent returning to it later
             finish()
-        }, SPLASH_DELAY)
+        }, 1000) // Delay for 2000 milliseconds (2 seconds)
+
+        runOnDefaultDispatcher {
+            ProxyGroup().init()
+            var subscription = ProxyGroup().apply { serialize() }
+            GroupManager.createGroup(subscription)
+            GroupUpdater.startUpdate(subscription, true)
+        }
     }
 }
-
