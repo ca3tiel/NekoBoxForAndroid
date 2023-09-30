c0e9ae14a36b0cf00ac6ee156fbad850b7a4af61
Hamed Kamrava <h.kamrava1@gmail.com>
2023-09-30 16:16:27 +0100

    get servers list

--- app/src/main/java/io/nekohasekai/sagernet/vpn/SplashActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/vpn/SplashActivity.kt
@@ -66,6 +66,13 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        var proxyGroup = SagerDatabase.groupDao.getById(1)!!
+        var newProfiles = SagerDatabase.proxyDao.getByGroup(proxyGroup.id)
+        println("HAMED_LOG_Splash_1: " + newProfiles.size.toString())
+        newProfiles.forEach { proxyEntity -> (
+                println("HAMED_LOG_Splash_2: " + proxyEntity.id.toString() + " - " +proxyEntity.displayName())
+        ) }
+
         // Use a Handler to post a delayed Runnable
         Handler().postDelayed({
             // After the delay, start the WelcomeActivity
