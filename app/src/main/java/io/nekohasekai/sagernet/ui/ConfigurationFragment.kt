01519c9edf44c32d1358dc9b9d4451358343c98c
Hamed Kamrava <h.kamrava1@gmail.com>
2023-10-01 23:48:33 +0100

    fix

--- app/src/main/java/io/nekohasekai/sagernet/ui/ConfigurationFragment.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/ConfigurationFragment.kt
@@ -1313,7 +1313,6 @@ class ConfigurationFragment @JvmOverloads constructor(
                         undoManager.flush()
                     }
                     val pos = itemCount
-                    println("HAMED_LOG_3: " + profile.id.toString())
                     configurationList[profile.id] = profile
                     configurationIdList.add(profile.id)
                     notifyItemInserted(pos)
@@ -1392,7 +1391,6 @@ class ConfigurationFragment @JvmOverloads constructor(
 
             fun reloadProfiles() {
                 var newProfiles = SagerDatabase.proxyDao.getByGroup(proxyGroup.id)
-                println("HAMED_LOG_4: " + newProfiles.size.toString())
                 val subscription = proxyGroup.subscription
                 when (proxyGroup.order) {
                     GroupOrder.BY_NAME -> {
@@ -1461,12 +1459,10 @@ class ConfigurationFragment @JvmOverloads constructor(
 
                 if (select) {
                     view.setOnClickListener {
-                        println("HAMED_LOG_15: " + proxyEntity.id.toString())
                         (requireActivity() as SelectCallback).returnProfile(proxyEntity.id)
                     }
                 } else {
                     view.setOnClickListener {
-                        println("HAMED_LOG_16: " + proxyEntity.id.toString())
                         runOnDefaultDispatcher {
                             var update: Boolean
                             var lastSelected: Long
@@ -1480,8 +1476,6 @@ class ConfigurationFragment @JvmOverloads constructor(
                             }
 
                             if (update) {
-                                println("HAMED_LOG_17: " + lastSelected.toString())
-                                println("HAMED_LOG_18: " + DataStore.selectedProxy)
                                 ProfileManager.postUpdate(lastSelected)
                                 if (DataStore.serviceState.canStop && reloadAccess.tryLock()) {
                                     SagerNet.reloadService()
