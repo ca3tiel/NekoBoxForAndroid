eec4646b0b53a8c64c774280b0da64099a7988c1
Hamed Kamrava <h.kamrava1@gmail.com>
2023-10-01 23:48:33 +0100

    fix

--- app/src/main/java/io/nekohasekai/sagernet/ui/ConfigurationFragment.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/ConfigurationFragment.kt
@@ -1142,10 +1142,7 @@ class ConfigurationFragment @JvmOverloads constructor(
             configurationListView.adapter = adapter
             configurationListView.setItemViewCacheSize(20)
 
-            println("HAMED_LOG_1: " +  configurationListView.childCount.toString())
-
             if (!select) {
-                println("HAMED_LOG_2")
                 undoManager = UndoSnackbarManager(activity as MainActivity, adapter!!)
 
                 ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
@@ -1326,7 +1323,6 @@ class ConfigurationFragment @JvmOverloads constructor(
                         undoManager.flush()
                     }
                     val pos = itemCount
-                    println("HAMED_LOG_3: " + profile.id.toString())
                     configurationList[profile.id] = profile
                     configurationIdList.add(profile.id)
                     notifyItemInserted(pos)
@@ -1405,7 +1401,6 @@ class ConfigurationFragment @JvmOverloads constructor(
 
             fun reloadProfiles() {
                 var newProfiles = SagerDatabase.proxyDao.getByGroup(proxyGroup.id)
-                println("HAMED_LOG_4: " + newProfiles.size.toString())
                 val subscription = proxyGroup.subscription
                 when (proxyGroup.order) {
                     GroupOrder.BY_NAME -> {
@@ -1474,12 +1469,10 @@ class ConfigurationFragment @JvmOverloads constructor(
 
                 if (select) {
                     view.setOnClickListener {
-                        println("HAMED_LOG_15: " + proxyEntity.id.toString())
                         (requireActivity() as SelectCallback).returnProfile(proxyEntity.id)
                     }
                 } else {
                     view.setOnClickListener {
-                        println("HAMED_LOG_16: " + proxyEntity.id.toString())
                         runOnDefaultDispatcher {
                             var update: Boolean
                             var lastSelected: Long
@@ -1493,8 +1486,6 @@ class ConfigurationFragment @JvmOverloads constructor(
                             }
 
                             if (update) {
-                                println("HAMED_LOG_17: " + lastSelected.toString())
-                                println("HAMED_LOG_18: " + DataStore.selectedProxy)
                                 ProfileManager.postUpdate(lastSelected)
                                 if (DataStore.serviceState.canStop && reloadAccess.tryLock()) {
                                     SagerNet.reloadService()
