251b858a140b075804224b78ee53d8963608339c
Ca3tiel <ahmad.mosallanezhad@gmail.com>
2023-10-05 06:55:13 +0330

    fix navigation menu UX

--- app/src/main/java/io/nekohasekai/sagernet/vpn/nav/CommentFragment.kt
+++ app/src/main/java/io/nekohasekai/sagernet/vpn/nav/CommentFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import io.nekohasekai.sagernet.R
 class CommentFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_comment, container, false)
+        val view = inflater.inflate(R.layout.fragment_comment, container, false)
+        val iconAngle = view.findViewById<ImageView>(R.id.ivCommentIconAngle)
+
+        // Add click listener to iconAngle
+        iconAngle.setOnClickListener {
+            // Pop the back stack to navigate back to the previous fragment
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        return view
     }
 }
