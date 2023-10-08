ecffb3fdb8094849014c343514f62daed2cdbb79
Hamed Kamrava <h.kamrava1@gmail.com>
2023-10-08 19:41:24 +0100

    add best server(WIP)

--- app/src/main/java/io/nekohasekai/sagernet/vpn/serverlist/ListItem.kt
+++ app/src/main/java/io/nekohasekai/sagernet/vpn/serverlist/ListItem.kt
@@ -1,6 +1,6 @@
 package io.nekohasekai.sagernet.vpn.serverlist
 
-data class ListItem(var name: String, val dropdownItems: MutableList<ListSubItem>, var isExpanded: Boolean = false, var iconResId: Int)
+data class ListItem(var name: String, val dropdownItems: MutableList<ListSubItem>, var isExpanded: Boolean = false, var iconResId: Int, var isBestServer: Boolean = false, var id: Long = -1, var isSelected: Boolean = false)
 {
     // Click listener function that can be set from outside the class
     private var clickListener: (() -> Unit)? = null
