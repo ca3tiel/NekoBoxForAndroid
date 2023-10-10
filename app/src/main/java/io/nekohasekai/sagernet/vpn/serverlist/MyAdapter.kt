b8965c7f42c2b2c810e1fa151f58e2388eae1f17
Ca3tiel <ahmad.mosallanezhad@gmail.com>
2023-10-10 19:47:14 +0330

    fix ping test issue and need duble click to open lists

--- app/src/main/java/io/nekohasekai/sagernet/vpn/serverlist/MyAdapter.kt
+++ app/src/main/java/io/nekohasekai/sagernet/vpn/serverlist/MyAdapter.kt
@@ -16,9 +16,11 @@ import kotlinx.coroutines.sync.Mutex
 
 class MyAdapter(
     private val itemList: List<ListItem>,
-    private val itemClickListener: (ListItem) -> Unit // Pass a lambda function as a parameter
+    private val itemClickListener: (ListItem) -> Unit
 ) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
     private var lastSelectedPosition: Int = RecyclerView.NO_POSITION
+
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val itemName: TextView = itemView.findViewById(R.id.itemName)
         val itemIcon: ImageView = itemView.findViewById(R.id.itemIcon)
@@ -29,16 +31,21 @@ class MyAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // Inflate the layout for the list item
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_item, parent, false)
         return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Get the data for the current item
         val item = itemList[position]
+
+        // Bind basic item information
         holder.itemName.text = item.name
         holder.itemIcon.setImageResource(item.iconResId)
 
+        // Bind expand/collapse state
         val isExpanded = item.isExpanded
         holder.expandIcon.setImageResource(if (isExpanded) R.drawable.ic_minus else R.drawable.ic_plus)
         holder.dropdownList.visibility = if (isExpanded) View.VISIBLE else View.GONE
@@ -46,15 +53,14 @@ class MyAdapter(
 
         // Set up click listener for the expand/collapse functionality
         holder.itemHeader.setOnClickListener {
-
-            if(item.isBestServer) {
+            if (item.isBestServer) {
                 holder.selectedView.visibility = View.VISIBLE
             } else {
                 item.isExpanded = !isExpanded
 
-                if (lastSelectedPosition != RecyclerView.NO_POSITION) {
-                    notifyItemChanged(lastSelectedPosition)
-                }
+                // Using notifyDataSetChanged for a smoother transition
+                notifyDataSetChanged()
+
                 holder.selectedView.visibility = View.INVISIBLE
             }
 
@@ -65,7 +71,9 @@ class MyAdapter(
             itemClickListener(item)
         }
 
-        holder.selectedView.visibility = if (lastSelectedPosition == position && item.isBestServer) View.VISIBLE else View.INVISIBLE
+        // Set visibility of selectedView based on the last selected position and isBestServer
+        holder.selectedView.visibility =
+            if (lastSelectedPosition == position && item.isBestServer) View.VISIBLE else View.INVISIBLE
 
         // Create and set a RecyclerView adapter for the dropdown list
         val dropdownAdapter = DropdownAdapter(item.dropdownItems) { clickedItem ->
@@ -80,6 +88,7 @@ class MyAdapter(
     }
 
     override fun getItemCount(): Int {
+        // Return the number of items in the list
         return itemList.size
     }
 }
