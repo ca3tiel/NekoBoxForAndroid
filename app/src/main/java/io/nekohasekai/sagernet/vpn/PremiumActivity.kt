af61ae4cc84e7593dbdfdc6088263ac7f3d3fbfe
Ca3tiel <ahmad.mosallanezhad@gmail.com>
2023-10-20 03:07:44 +0330

    fix Premium layout

--- app/src/main/java/io/nekohasekai/sagernet/vpn/PremiumActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/vpn/PremiumActivity.kt
@@ -1,12 +1,23 @@
 package io.nekohasekai.sagernet.vpn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import io.nekohasekai.sagernet.R
 
 class PremiumActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_premium)
+
+        val ivIconAngle = findViewById<ImageView>(R.id.ivIconAngle)
+        ivIconAngle.setOnClickListener { navigateToDashboard() }
+    }
+
+    private fun navigateToDashboard() {
+        val intent = Intent(this, DashboardActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
