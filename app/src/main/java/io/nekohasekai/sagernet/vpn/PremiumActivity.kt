6ff357ec1d9cadd65e02e82a6cb08ec20cf83fe4
Ca3tiel <ahmad.mosallanezhad@gmail.com>
2023-10-19 21:45:49 +0330

    make premium activity

---
+++ app/src/main/java/io/nekohasekai/sagernet/vpn/PremiumActivity.kt
@@ -0,0 +1,12 @@
+package io.nekohasekai.sagernet.vpn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.nekohasekai.sagernet.R
+
+class PremiumActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_premium)
+    }
+}
