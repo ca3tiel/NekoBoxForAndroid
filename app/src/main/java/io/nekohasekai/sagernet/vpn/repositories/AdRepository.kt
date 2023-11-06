a8f4539b5158ffd5240b0a21ce404f51b4da6f0c
Hamed Kamrava <h.kamrava1@gmail.com>
2023-11-06 23:18:35 +0000

    EU consent WIP

--- app/src/main/java/io/nekohasekai/sagernet/vpn/repositories/AdRepository.kt
+++ app/src/main/java/io/nekohasekai/sagernet/vpn/repositories/AdRepository.kt
@@ -3,8 +3,6 @@ package io.nekohasekai.sagernet.vpn.repositories
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
-import android.os.Handler
-import android.util.Log
 import com.google.android.gms.ads.AdError
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.FullScreenContentCallback
@@ -13,18 +11,26 @@ import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.OnUserEarnedRewardListener
 import com.google.android.gms.ads.rewarded.RewardedAd
 import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
-import io.nekohasekai.sagernet.R
-import io.nekohasekai.sagernet.vpn.AD_REWARDED_UNIT_ID
-import io.nekohasekai.sagernet.vpn.TAG
+import com.google.android.ump.ConsentForm
+import com.google.android.ump.ConsentInformation
+import com.google.android.ump.ConsentRequestParameters
+import com.google.android.ump.UserMessagingPlatform
 import io.nekohasekai.sagernet.vpn.utils.InternetConnectionChecker
+import java.util.concurrent.atomic.AtomicBoolean
 
 object AdRepository {
     private var rewardedAd: RewardedAd? = null
     private var rewardedAdToken: String? = null
+    private lateinit var consentInformation: ConsentInformation
+    private var isMobileAdsInitializeCalled = AtomicBoolean(false)
+
     @SuppressLint("StaticFieldLeak")
     lateinit var internetChecker: InternetConnectionChecker
 
     fun loadRewardedAd(context: Context) {
+        if(!canRequestAds()) {
+            return
+        }
         var adRequest = AdRequest.Builder().build()
         RewardedAd.load(context,"ca-app-pub-3940256099942544/5224354917", adRequest, object : RewardedAdLoadCallback() {
             override fun onAdFailedToLoad(adError: LoadAdError) {
@@ -44,8 +50,11 @@ object AdRepository {
     }
 
     fun showRewardedAd(context: Context) {
+        if(!canRequestAds()) {
+            return
+        }
         if(!AppRepository.isInternetAvailable()) {
-            return;
+            return
         }
         if (rewardedAd != null) {
             rewardedAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
@@ -89,6 +98,68 @@ object AdRepository {
 
             }
         }
+    }
+
+    fun checkAdConsent(context: Context) {
+        // Set tag for under age of consent. false means users are not under age
+        // of consent.
+        val params = ConsentRequestParameters
+            .Builder()
+            .setTagForUnderAgeOfConsent(false)
+            .build()
+
+        consentInformation = UserMessagingPlatform.getConsentInformation(context)
+        consentInformation.requestConsentInfoUpdate(
+            (context as Activity),
+            params,
+            ConsentInformation.OnConsentInfoUpdateSuccessListener {
+                UserMessagingPlatform.loadAndShowConsentFormIfRequired(
+                    context,
+                    ConsentForm.OnConsentFormDismissedListener {
+                            loadAndShowError ->
+                        // Consent gathering failed.
+                        println("HAMED_LOG_loadAndShowError: " + loadAndShowError?.message)
+                        // Consent has been gathered.
+                        if (consentInformation.canRequestAds()) {
+                            initializeMobileAdsSdk(context)
+                        }
+                    }
+                )
+            },
+            ConsentInformation.OnConsentInfoUpdateFailureListener {
+                    requestConsentError ->
+                // Consent gathering failed.
+                println("HAMED_LOG_requestConsentError: " + requestConsentError.message)
+//                Log.w(TAG, String.format("%s: %s",
+//                    requestConsentError.errorCode(),
+//                    requestConsentError.message()))
+            })
+    }
+
+    private fun initializeMobileAdsSdk(context: Context) {
+        if (isMobileAdsInitializeCalled.get()) {
+            return
+        }
+        isMobileAdsInitializeCalled.set(true)
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.initialize(context)
+
+        // TODO: Request an ad.
+        // InterstitialAd.load(...)
+    }
+
+    fun checkInitializeMobileAdsSdk(context: Context) {
+        // Check if you can initialize the Google Mobile Ads SDK in parallel
+        // while checking for new consent information. Consent obtained in
+        // the previous session can be used to request ads.
+        if (consentInformation.canRequestAds()) {
+            initializeMobileAdsSdk(context)
+        }
+    }
+
+    fun canRequestAds(): Boolean {
+        return consentInformation.canRequestAds()
     }
 
 //    fun loadBannerAd(context: Context) {
