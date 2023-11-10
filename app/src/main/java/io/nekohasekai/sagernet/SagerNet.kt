ede0dce9a5b6be2fa0349d7d0365d9c73ca6193e
Hamed Kamrava <h.kamrava1@gmail.com>
2023-11-10 18:13:32 +0000

    fix app open ufffff

--- app/src/main/java/io/nekohasekai/sagernet/SagerNet.kt
+++ app/src/main/java/io/nekohasekai/sagernet/SagerNet.kt
@@ -10,19 +10,35 @@ import android.content.res.Configuration
 import android.net.ConnectivityManager
 import android.net.Network
 import android.os.Build
+import android.os.Bundle
 import android.os.PowerManager
 import android.os.StrictMode
 import android.os.UserManager
+import android.util.Log
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.core.content.getSystemService
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.appopen.AppOpenAd
 import go.Seq
+import androidx.lifecycle.ProcessLifecycleOwner
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.FullScreenContentCallback
 import io.nekohasekai.sagernet.bg.SagerConnection
 import io.nekohasekai.sagernet.database.DataStore
 import io.nekohasekai.sagernet.ktx.Logs
 import io.nekohasekai.sagernet.ktx.runOnDefaultDispatcher
 import io.nekohasekai.sagernet.ui.MainActivity
 import io.nekohasekai.sagernet.utils.*
+import io.nekohasekai.sagernet.vpn.repositories.AdRepository
+import io.nekohasekai.sagernet.vpn.repositories.AdRepository.appOpenAdManager
+import io.nekohasekai.sagernet.vpn.repositories.AppRepository
 import kotlinx.coroutines.DEBUG_PROPERTY_NAME
 import kotlinx.coroutines.DEBUG_PROPERTY_VALUE_ON
 import libcore.Libcore
@@ -30,10 +46,45 @@ import moe.matsuri.nb4a.NativeInterface
 import moe.matsuri.nb4a.utils.JavaUtil
 import moe.matsuri.nb4a.utils.cleanWebview
 import java.io.File
+import java.util.Date
 import androidx.work.Configuration as WorkConfiguration
 
 class SagerNet : Application(),
-    WorkConfiguration.Provider {
+    WorkConfiguration.Provider, Application.ActivityLifecycleCallbacks, LifecycleObserver {
+    private val LOG_TAG: String = "HAMED_LOG_SagerNet"
+    private var currentActivity: Activity? = null
+
+    /** ActivityLifecycleCallback methods. */
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
+
+    override fun onActivityStarted(activity: Activity) {
+        // Updating the currentActivity only when an ad is not showing.
+        if (!appOpenAdManager.isShowingAd) {
+            currentActivity = activity
+        }
+    }
+
+    override fun onActivityResumed(activity: Activity) {}
+
+    override fun onActivityPaused(activity: Activity) {}
+
+    override fun onActivityStopped(activity: Activity) {}
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+
+    override fun onActivityDestroyed(activity: Activity) {}
+
+    /**
+     * Shows an app open ad.
+     *
+     * @param activity the activity that shows the app open ad
+     * @param onShowAdCompleteListener the listener to be notified when an app open ad is complete
+     */
+    fun showAdIfAvailable(activity: Activity, onShowAdCompleteListener: OnShowAdCompleteListener) {
+        // We wrap the showAdIfAvailable to enforce that other classes only interact with MyApplication
+        // class.
+        appOpenAdManager.showAdIfAvailable(activity, onShowAdCompleteListener)
+    }
 
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
@@ -97,6 +148,21 @@ class SagerNet : Application(),
                 .penaltyLog()
                 .build()
         )
+
+        // Ads
+        registerActivityLifecycleCallbacks(this)
+        MobileAds.initialize(this) {}
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+        appOpenAdManager = AppOpenAdManager()
+    }
+
+    /** LifecycleObserver method that shows the app open ad when the app moves to foreground. */
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onMoveToForeground() {
+        // Show the ad (if available) when the app moves to foreground.
+        currentActivity?.let {
+            appOpenAdManager.showAdIfAvailable(it)
+        }
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
@@ -119,6 +185,7 @@ class SagerNet : Application(),
     @SuppressLint("InlinedApi")
     companion object {
 
+        @SuppressLint("StaticFieldLeak")
         lateinit var application: SagerNet
 
         val isTv by lazy {
@@ -195,5 +262,147 @@ class SagerNet : Application(),
         var underlyingNetwork: Network? = null
 
     }
+
+    /** Interface definition for a callback to be invoked when an app open ad is complete. */
+    interface OnShowAdCompleteListener {
+        fun onShowAdComplete()
+    }
+
+    /** Inner class that loads and shows app open ads. */
+    inner class AppOpenAdManager {
+        private var isLoadingAd = false
+        var isShowingAd = false
+        /** Keep track of the time an app open ad is loaded to ensure you don't show an expired ad. */
+        private var loadTime: Long = 0
+
+
+        /** Request an ad. */
+        fun loadAd(context: Context) {
+            println("HAMED_LOG_Start_Loading_AppOpen_Ad_0")
+            // Do not load ad if there is an unused ad or one is already loading.
+            if (isLoadingAd || isAdAvailable()) {
+                return
+            }
+
+            isLoadingAd = true
+            val request = AdRequest.Builder().build()
+            println("HAMED_LOG_Start_Loading_AppOpen_Ad_1")
+            AppOpenAd.load(
+                context, AdRepository.getAppOpenToken(), request,
+                AppOpenAd.APP_OPEN_AD_ORIENTATION_PORTRAIT,
+                object : AppOpenAd.AppOpenAdLoadCallback() {
+
+                    override fun onAdLoaded(ad: AppOpenAd) {
+                        // Called when an app open ad has loaded.
+                        println("HAMED_LOG_Ad was loaded.")
+                        AdRepository.appOpenAd = ad
+                        isLoadingAd = false
+                        loadTime = Date().time
+                    }
+
+                    override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                        // Called when an app open ad has failed to load.
+                        println("HAMED_LOG_APP_OPEN: " + loadAdError.message)
+
+                        isLoadingAd = false;
+                    }
+                })
+        }
+
+        private fun wasLoadTimeLessThanNHoursAgo(numHours: Long): Boolean {
+            val dateDifference: Long = Date().time - loadTime
+            val numMilliSecondsPerHour: Long = 3600000
+            return dateDifference < numMilliSecondsPerHour * numHours
+        }
+
+        /** Check if ad exists and can be shown. */
+        private fun isAdAvailable(): Boolean {
+            Log.d(LOG_TAG, AdRepository.appOpenAd.toString())
+//            return AdRepository.appOpenAd != null && wasLoadTimeLessThanNHoursAgo(4)
+            return AdRepository.appOpenAd != null
+        }
+
+        /**
+         * Show the ad if one isn't already showing.
+         *
+         * @param activity the activity that shows the app open ad
+         */
+        fun showAdIfAvailable(activity: Activity) {
+            showAdIfAvailable(
+                activity,
+                object : OnShowAdCompleteListener {
+                    override fun onShowAdComplete() {
+                        // Empty because the user will go back to the activity that shows the ad.
+                        println("HAMED_LOG_APP_OPEN_onShowAdComplete")
+                    }
+                }
+            )
+        }
+
+        /**
+         * Show the ad if one isn't already showing.
+         *
+         * @param activity the activity that shows the app open ad
+         * @param onShowAdCompleteListener the listener to be notified when an app open ad is complete
+         */
+        fun showAdIfAvailable(activity: Activity, onShowAdCompleteListener: OnShowAdCompleteListener) {
+            // If the app open ad is already showing, do not show the ad again.
+            if (isShowingAd) {
+                Log.d(LOG_TAG, "The app open ad is already showing.")
+                return
+            }
+
+            // If the app open ad is not available yet, invoke the callback.
+            if (!isAdAvailable()) {
+                Log.d(LOG_TAG, "The app open ad is not ready yet.")
+                onShowAdCompleteListener.onShowAdComplete()
+                if (AdRepository.consentInformation.canRequestAds()) {
+                    loadAd(activity)
+                }
+                return
+            }
+
+            Log.d(LOG_TAG, "Will show ad.")
+
+            AdRepository.appOpenAd?.fullScreenContentCallback =
+                object : FullScreenContentCallback() {
+                    /** Called when full screen content is dismissed. */
+                    override fun onAdDismissedFullScreenContent() {
+                        // Set the reference to null so isAdAvailable() returns false.
+                        AdRepository.appOpenAd = null
+                        isShowingAd = false
+                        AppRepository.DebugLog("onAdDismissedFullScreenContent.")
+                        Toast.makeText(activity, "onAdDismissedFullScreenContent", Toast.LENGTH_SHORT).show()
+
+                        onShowAdCompleteListener.onShowAdComplete()
+                        if (AdRepository.consentInformation.canRequestAds()) {
+                            loadAd(activity)
+                        }
+                    }
+
+                    /** Called when fullscreen content failed to show. */
+                    override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                        AdRepository.appOpenAd = null
+                        isShowingAd = false
+                        Log.d(LOG_TAG, "onAdFailedToShowFullScreenContent: " + adError.message)
+                        Toast.makeText(activity, "onAdFailedToShowFullScreenContent", Toast.LENGTH_SHORT).show()
+
+                        onShowAdCompleteListener.onShowAdComplete()
+                        if (AdRepository.consentInformation.canRequestAds()) {
+                            loadAd(activity)
+                        }
+                    }
+
+                    /** Called when fullscreen content is shown. */
+                    override fun onAdShowedFullScreenContent() {
+                        Log.d(LOG_TAG, "onAdShowedFullScreenContent.")
+                        Toast.makeText(activity, "onAdShowedFullScreenContent", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            isShowingAd = true
+            AdRepository.appOpenAd?.show(activity)
+        }
+    }
+
 
 }
