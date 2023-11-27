728320fa95b2d3ff8ba51ba962f7837a62e343b0
Ca3tiel <ahmad.mosallanezhad@gmail.com>
2023-11-28 00:37:59 +0330

    fix welcome activity translate for all languages and add code for AccountFragment

--- app/src/main/java/io/nekohasekai/sagernet/vpn/nav/AccountFragment.kt
+++ app/src/main/java/io/nekohasekai/sagernet/vpn/nav/AccountFragment.kt
@@ -1,12 +1,18 @@
 package io.nekohasekai.sagernet.vpn.nav
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import io.nekohasekai.sagernet.R
+import io.nekohasekai.sagernet.vpn.WelcomeActivity
+import io.nekohasekai.sagernet.vpn.repositories.SocialAuthRepository
+
 class AccountFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,11 +20,28 @@ class AccountFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_account, container, false)
         val iconAngle = view.findViewById<ImageView>(R.id.ivAccountIconAngle)
+        val emailTextView: TextView = view.findViewById(R.id.tvEmail)
+        val signOutButton: Button = view.findViewById(R.id.btnExitAccount)
 
-        // Add click listener to iconAngle
+        // Your existing code for the iconAngle click listener
         iconAngle.setOnClickListener {
             // Pop the back stack to navigate back to the previous fragment
             requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        // Add the provided code
+        val currentUser = SocialAuthRepository.firebaseAuth.currentUser
+        if (currentUser != null) {
+            emailTextView.text = "${currentUser.email}"
+        }
+
+        signOutButton.setOnClickListener {
+            SocialAuthRepository.facebookLoginManager.logOut()
+            SocialAuthRepository.firebaseAuth.signOut()
+            SocialAuthRepository.googleSignInClient.signOut()
+            val intent = Intent(requireContext(), WelcomeActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
         }
 
         return view
