89e51859372d801af5c228d92f67bdbe207d35f3
Hamed Kamrava <h.kamrava1@gmail.com>
2024-04-21 00:10:55 +0100

    cleanup

--- app/src/main/java/io/nekohasekai/sagernet/vpn/ForceUpdateActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/vpn/ForceUpdateActivity.kt
@@ -4,7 +4,6 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
 import io.nekohasekai.sagernet.R
 import io.nekohasekai.sagernet.databinding.ActivityForceUpdateBinding
 import io.nekohasekai.sagernet.ui.ThemedActivity
