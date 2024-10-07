25ba3338e94e308a3887ca74baf59c5e52adcb3a
Ca3tiel <ahmad.mosallanezhad@gmail.com>
2024-10-07 11:29:34 +0330

    Copied changed files from develop to empty diff branch

---
+++ app/src/main/java/io/nekohasekai/sagernet/SagerNet.kt
@@ -0,0 +1,413 @@
+package io.nekohasekai.sagernet
+
+import android.annotation.SuppressLint
+import android.app.*
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
+import android.net.ConnectivityManager
+import android.net.Network
+import android.os.Build
+import android.os.Bundle
+import android.os.PowerManager
+import android.os.StrictMode
+import android.os.UserManager
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.appopen.AppOpenAd
+import go.Seq
+import androidx.lifecycle.ProcessLifecycleOwner
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.FullScreenContentCallback
+import io.nekohasekai.sagernet.bg.SagerConnection
+import io.nekohasekai.sagernet.database.DataStore
+import io.nekohasekai.sagernet.ktx.Logs
+import io.nekohasekai.sagernet.ktx.runOnDefaultDispatcher
+import io.nekohasekai.sagernet.ui.MainActivity
+import io.nekohasekai.sagernet.utils.*
+import io.nekohasekai.sagernet.vpn.di.appModule
+import io.nekohasekai.sagernet.vpn.repositories.AdRepository
+import io.nekohasekai.sagernet.vpn.repositories.AdRepository.appOpenAdManager
+import io.nekohasekai.sagernet.vpn.repositories.AppRepository
+import kotlinx.coroutines.DEBUG_PROPERTY_NAME
+import kotlinx.coroutines.DEBUG_PROPERTY_VALUE_ON
+import libcore.Libcore
+import moe.matsuri.nb4a.NativeInterface
+import moe.matsuri.nb4a.utils.JavaUtil
+import moe.matsuri.nb4a.utils.cleanWebview
+import java.io.File
+import java.util.Date
+import androidx.work.Configuration as WorkConfiguration
+import org.koin.core.context.startKoin
+
+class SagerNet : Application(),
+    WorkConfiguration.Provider, Application.ActivityLifecycleCallbacks, LifecycleObserver {
+    private val LOG_TAG: String = "HAMED_LOG_SagerNet"
+    private var currentActivity: Activity? = null
+
+    /** ActivityLifecycleCallback methods. */
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
+
+    override fun onActivityStarted(activity: Activity) {
+        // Updating the currentActivity only when an ad is not showing.
+        if (!appOpenAdManager.isShowingAd) {
+            currentActivity = activity
+        }
+    }
+
+    override fun onActivityResumed(activity: Activity) {}
+
+    override fun onActivityPaused(activity: Activity) {}
+
+    override fun onActivityStopped(activity: Activity) {}
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+
+    override fun onActivityDestroyed(activity: Activity) {}
+
+    /**
+     * Shows an app open ad.
+     *
+     * @param activity the activity that shows the app open ad
+     * @param onShowAdCompleteListener the listener to be notified when an app open ad is complete
+     */
+    fun showAdIfAvailable(activity: Activity, onShowAdCompleteListener: OnShowAdCompleteListener) {
+        // We wrap the showAdIfAvailable to enforce that other classes only interact with MyApplication
+        // class.
+        appOpenAdManager.showAdIfAvailable(activity, onShowAdCompleteListener)
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+
+        application = this
+    }
+
+    private val nativeInterface = NativeInterface()
+
+    val externalAssets: File by lazy { getExternalFilesDir(null) ?: filesDir }
+    val process: String = JavaUtil.getProcessName()
+    private val isMainProcess = process == BuildConfig.APPLICATION_ID
+    val isBgProcess = process.endsWith(":bg")
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            modules(appModule)
+        }
+
+        System.setProperty(DEBUG_PROPERTY_NAME, DEBUG_PROPERTY_VALUE_ON)
+        Thread.setDefaultUncaughtExceptionHandler(CrashHandler)
+
+        if (isMainProcess || isBgProcess) {
+            // fix multi process issue in Android 9+
+            JavaUtil.handleWebviewDir(this)
+
+            runOnDefaultDispatcher {
+                PackageCache.register()
+                cleanWebview()
+            }
+        }
+
+        Seq.setContext(this)
+        updateNotificationChannels()
+
+        // nb4a: init core
+        externalAssets.mkdirs()
+        Libcore.initCore(
+            process,
+            cacheDir.absolutePath + "/",
+            filesDir.absolutePath + "/",
+            externalAssets.absolutePath + "/",
+            DataStore.logBufSize,
+            DataStore.logLevel > 0,
+            nativeInterface, nativeInterface
+        )
+
+        if (isMainProcess) {
+            Theme.apply(this)
+            Theme.applyNightTheme()
+            runOnDefaultDispatcher {
+                DefaultNetworkListener.start(this) {
+                    underlyingNetwork = it
+                }
+            }
+        }
+
+        if (BuildConfig.DEBUG) StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .detectLeakedRegistrationObjects()
+                .penaltyLog()
+                .build()
+        )
+
+        // Ads
+        registerActivityLifecycleCallbacks(this)
+        MobileAds.initialize(this) {}
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+        appOpenAdManager = AppOpenAdManager()
+    }
+
+    /** LifecycleObserver method that shows the app open ad when the app moves to foreground. */
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onMoveToForeground() {
+        // Show the ad (if available) when the app moves to foreground.
+        currentActivity?.let {
+            appOpenAdManager.showAdIfAvailable(it)
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        updateNotificationChannels()
+    }
+
+    override fun getWorkManagerConfiguration(): WorkConfiguration {
+        return WorkConfiguration.Builder()
+            .setDefaultProcessName("${BuildConfig.APPLICATION_ID}:bg")
+            .build()
+    }
+
+    override fun onTrimMemory(level: Int) {
+        super.onTrimMemory(level)
+
+        Libcore.forceGc()
+    }
+
+    @SuppressLint("InlinedApi")
+    companion object {
+
+        @SuppressLint("StaticFieldLeak")
+        lateinit var application: SagerNet
+
+        val isTv by lazy {
+            uiMode.currentModeType == Configuration.UI_MODE_TYPE_TELEVISION
+        }
+
+        val configureIntent: (Context) -> PendingIntent by lazy {
+            {
+                PendingIntent.getActivity(
+                    it,
+                    0,
+                    Intent(
+                        application, MainActivity::class.java
+                    ).setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT),
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
+                )
+            }
+        }
+        val activity by lazy { application.getSystemService<ActivityManager>()!! }
+        val clipboard by lazy { application.getSystemService<ClipboardManager>()!! }
+        val connectivity by lazy { application.getSystemService<ConnectivityManager>()!! }
+        val notification by lazy { application.getSystemService<NotificationManager>()!! }
+        val user by lazy { application.getSystemService<UserManager>()!! }
+        val uiMode by lazy { application.getSystemService<UiModeManager>()!! }
+        val power by lazy { application.getSystemService<PowerManager>()!! }
+
+        fun getClipboardText(): String {
+            return clipboard.primaryClip?.takeIf { it.itemCount > 0 }
+                ?.getItemAt(0)?.text?.toString() ?: ""
+        }
+
+        fun trySetPrimaryClip(clip: String) = try {
+            clipboard.setPrimaryClip(ClipData.newPlainText(null, clip))
+            true
+        } catch (e: RuntimeException) {
+            Logs.w(e)
+            false
+        }
+
+        fun updateNotificationChannels() {
+            if (Build.VERSION.SDK_INT >= 26) @RequiresApi(26) {
+                notification.createNotificationChannels(
+                    listOf(
+                        NotificationChannel(
+                            "service-vpn",
+                            application.getText(R.string.service_vpn),
+                            if (Build.VERSION.SDK_INT >= 28) NotificationManager.IMPORTANCE_MIN
+                            else NotificationManager.IMPORTANCE_LOW
+                        ),   // #1355
+                        NotificationChannel(
+                            "service-proxy",
+                            application.getText(R.string.service_proxy),
+                            NotificationManager.IMPORTANCE_LOW
+                        ), NotificationChannel(
+                            "service-subscription",
+                            application.getText(R.string.service_subscription),
+                            NotificationManager.IMPORTANCE_DEFAULT
+                        )
+                    )
+                )
+            }
+        }
+
+        fun startService() = ContextCompat.startForegroundService(
+            application, Intent(application, SagerConnection.serviceClass)
+        )
+
+        fun reloadService() =
+            application.sendBroadcast(Intent(Action.RELOAD).setPackage(application.packageName))
+
+        fun stopService() =
+            application.sendBroadcast(Intent(Action.CLOSE).setPackage(application.packageName))
+
+        var underlyingNetwork: Network? = null
+
+    }
+
+    /** Interface definition for a callback to be invoked when an app open ad is complete. */
+    interface OnShowAdCompleteListener {
+        fun onShowAdComplete()
+    }
+
+    /** Inner class that loads and shows app open ads. */
+    inner class AppOpenAdManager {
+        private var isLoadingAd = false
+        var isShowingAd = false
+        /** Keep track of the time an app open ad is loaded to ensure you don't show an expired ad. */
+        private var loadTime: Long = 0
+
+
+        /** Request an ad. */
+        fun loadAd(context: Context) {
+            println("HAMED_LOG_Start_Loading_AppOpen_Ad_0")
+            // Do not load ad if there is an unused ad or one is already loading.
+            if (isLoadingAd || isAdAvailable()) {
+                return
+            }
+
+            isLoadingAd = true
+            val request = AdRequest.Builder().build()
+            println("HAMED_LOG_Start_Loading_AppOpen_Ad_1")
+            AppOpenAd.load(
+                context, AdRepository.getAppOpenAdUnitID(), request,
+                AppOpenAd.APP_OPEN_AD_ORIENTATION_PORTRAIT,
+                object : AppOpenAd.AppOpenAdLoadCallback() {
+
+                    override fun onAdLoaded(ad: AppOpenAd) {
+                        // Called when an app open ad has loaded.
+                        println("HAMED_LOG_Ad was loaded.")
+                        AdRepository.appOpenAd = ad
+                        isLoadingAd = false
+                        loadTime = Date().time
+                    }
+
+                    override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                        // Called when an app open ad has failed to load.
+                        println("HAMED_LOG_APP_OPEN: " + loadAdError.message)
+
+                        isLoadingAd = false;
+                    }
+                })
+        }
+
+        private fun wasLoadTimeLessThanNHoursAgo(numHours: Long): Boolean {
+            val dateDifference: Long = Date().time - loadTime
+            val numMilliSecondsPerHour: Long = 3600000
+            return dateDifference < numMilliSecondsPerHour * numHours
+        }
+
+        /** Check if ad exists and can be shown. */
+        private fun isAdAvailable(): Boolean {
+            Log.d(LOG_TAG, AdRepository.appOpenAd.toString())
+//            return AdRepository.appOpenAd != null && wasLoadTimeLessThanNHoursAgo(4)
+            return AdRepository.appOpenAd != null
+        }
+
+        /**
+         * Show the ad if one isn't already showing.
+         *
+         * @param activity the activity that shows the app open ad
+         */
+        fun showAdIfAvailable(activity: Activity) {
+            showAdIfAvailable(
+                activity,
+                object : OnShowAdCompleteListener {
+                    override fun onShowAdComplete() {
+                        // Empty because the user will go back to the activity that shows the ad.
+                        println("HAMED_LOG_APP_OPEN_onShowAdComplete")
+                    }
+                }
+            )
+        }
+
+        /**
+         * Show the ad if one isn't already showing.
+         *
+         * @param activity the activity that shows the app open ad
+         * @param onShowAdCompleteListener the listener to be notified when an app open ad is complete
+         */
+        fun showAdIfAvailable(activity: Activity, onShowAdCompleteListener: OnShowAdCompleteListener) {
+            // If the app open ad is already showing, do not show the ad again.
+            if (isShowingAd) {
+                Log.d(LOG_TAG, "The app open ad is already showing.")
+                return
+            }
+
+            // If the app open ad is not available yet, invoke the callback.
+            if (!isAdAvailable()) {
+                Log.d(LOG_TAG, "The app open ad is not ready yet.")
+                onShowAdCompleteListener.onShowAdComplete()
+                if (AdRepository.consentInformation.canRequestAds()) {
+                    loadAd(activity)
+                }
+                return
+            }
+
+            Log.d(LOG_TAG, "Will show ad.")
+
+            AdRepository.appOpenAd?.fullScreenContentCallback =
+                object : FullScreenContentCallback() {
+                    /** Called when full screen content is dismissed. */
+                    override fun onAdDismissedFullScreenContent() {
+                        // Set the reference to null so isAdAvailable() returns false.
+                        AdRepository.appOpenAd = null
+                        isShowingAd = false
+                        Toast.makeText(activity, "onAdDismissedFullScreenContent", Toast.LENGTH_SHORT).show()
+
+                        onShowAdCompleteListener.onShowAdComplete()
+                        if (AdRepository.consentInformation.canRequestAds()) {
+                            loadAd(activity)
+                        }
+                    }
+
+                    /** Called when fullscreen content failed to show. */
+                    override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                        AdRepository.appOpenAd = null
+                        isShowingAd = false
+                        Log.d(LOG_TAG, "onAdFailedToShowFullScreenContent: " + adError.message)
+                        Toast.makeText(activity, "onAdFailedToShowFullScreenContent", Toast.LENGTH_SHORT).show()
+
+                        onShowAdCompleteListener.onShowAdComplete()
+                        if (AdRepository.consentInformation.canRequestAds()) {
+                            loadAd(activity)
+                        }
+                    }
+
+                    /** Called when fullscreen content is shown. */
+                    override fun onAdShowedFullScreenContent() {
+                        Log.d(LOG_TAG, "onAdShowedFullScreenContent.")
+                        Toast.makeText(activity, "onAdShowedFullScreenContent", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            isShowingAd = true
+            AdRepository.appOpenAd?.show(activity)
+        }
+    }
+
+
+}
---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven(url = "https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+    }
+}
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+allprojects {
+    apply(from = "${rootProject.projectDir}/repositories.gradle.kts")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
